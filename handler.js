f31d770366ef79fdfb564326b35c7b3b92981f56
preethis89 <preethis89@outlook.com>
2020-05-05 01:16:00 +0100

    Implemented user Login

--- handler.js
+++ handler.js
@@ -1,26 +1,41 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const cors = require("cors");
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "FD",
+  host: 'foodometer.cirzv0ojbs7b.eu-west-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Techreturners',
+  database: 'FD',
 });
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/foodometer", function(req, res) {
-  connection.query("SELECT * FROM task", function (err, data) {
-});
 
-app.post("/foodometer",(req,res)=>{
-  
-})
+app.post('/foodometer', (request, response) => {
+  const data = request.body;
+  if (data.username && data.password) {
+    connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [data.username, data.password], (error, results) => {
+      if (error) {
+        response.send({
+          code: 400,
+          failed: 'error ocurred',
+          error,
+        });
+      } else {
+        if (results.length > 0) {
+          response.send('Login Successful');
+        } else {
+          response.send('Incorrect Username and/or Password!');
+        }
+        response.end();
+      }
+    });
+  }
+});
 
 module.exports.app = serverless(app);
